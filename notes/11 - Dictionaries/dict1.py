bd96aa2b01848f5824c998eb5adfa5de11a70ef4
pguse <pguse@albertcollege.ca>
2022-03-29 16:50:58 -0400

    version 1

---
+++ notes/11 - Dictionaries/dict1.py
@@ -0,0 +1,8 @@
+courses = { "Math" : 75, "English" : 90, "History" : 85, "French" : 70, "Geography" : 80, "Science" : 95}
+
+# Adding another key-value pair to our dictionary
+courses["Spanish"] = 75
+print(courses)
+
+# Print a value given a key
+print( courses["Science"] )
